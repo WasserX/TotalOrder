4cfc096a464d83f76824a7434ce7271a758a3fef
Wasser <wasserx@hotmail.com>
2013-01-09 20:51:49 +0100

    Added file parsing

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ TotalOrder
 ==========
 
 Total Order Message Sending Simulator for the Distributed System Course
+
+Instructions:
+Uses Python 2.7
+
+To execute create a settings file in the format:
+<BCTREE|BCUNI|BCPIPE|TOLAT|TOTHROUGH>
+<number of processes to simulate>
+[turn pid]
+...
+[turn pid]
+
+
+BCTREE => Simulate a broadcast using a tree algorithm to distribute
+BCUNI =>  Basic broadcast where the original sender sends to all recipients
+BCPIPE => Broadcast where the sender only sends to the next one, and so on.
+TOLAT =>  Total Order Broadcast optimized to reduce latency
+TOTHROUGH => Total Order Broadcast optimized to increase throughput
+ 
+(turn, pid) is a pair where turn specifies the round where pid will try to start sending a new message
--- simulator.py
+++ simulator.py
@@ -1,18 +1,19 @@
 from process import *
-import random
+import argparse
 
 class Simulator:
     def __init__(self):
         self.nproc = 0
 
-    def simulate(self, mode, nproc):
+    def simulate(self, mode, nproc, new_msgs_schedule):
         """Simulates a broadcast or a Total Order Broadcast. Available modes are
         'BCUNI', 'BCTREE', 'BCPIPE', 'TOLAT', 'TOTHROUGH'. 
         nproc defines the number of processes in the simulation"""
         
-        self.nproc = nproc
-        self.processes = []
-        self.send_queue = []
+        self.nproc = nproc #Number of processes to simulate
+        self.new_msgs_schedule = new_msgs_schedule #When should a process send a new message. If Already sending, will enqueue. Format: [(turn, pid)]
+        self.processes = [] #References to processes
+        self.send_queue = [] #Internal list used to distribute messages
         
         if mode == 'BCUNI':
             for i in range(0, self.nproc):
@@ -40,17 +41,24 @@ class Simulator:
 
     def sim_broadcast(self):
         """Broadcasts a msg without acks using the processes algorithm to spread"""
-
-        #Sender of the packet
-        senders = [[random.randrange(self.nproc), 0]]
-        self.processes[senders[0][0]].send_new = True #Set flag to tell sender to create new msg
         
+        
+        #Order list of new msgs
+        self.new_msgs_schedule.sort()
+
         #Execute rounds
         turn = 0
         working = True
+        msg_latencies = []
         while working:
             turn = turn +1
             print '-- Round ' + str(turn) + ' --'
+
+            #See if a new msg needs to be created in this round and set flags accordingly
+            for msg_turn, pid in self.new_msgs_schedule:
+                if msg_turn == turn:
+                    self.processes[pid].send_new = True #Set flag to tell sender to create new msg
+                    msg_latencies.append([self.processes[pid].clock, 0])
 
             for proc in self.processes:
                 #Execute round for each process
@@ -66,14 +74,14 @@ class Simulator:
                 
             if working:
                 #Increase latencies
-                for sender in senders:
-                    sender[1] = sender[1] + 1
+                for latency in msg_latencies:
+                    latency[1] = latency[1] + 1
 
-        latency = -1
-        for sender in senders:
-            latency = sender[1] if latency < sender[1] else latency
+        biggest_latency = -1
+        for latency in msg_latencies:
+            biggest_latency = latency[1] if biggest_latency < latency[1] else biggest_latency
 
-        self.print_results(self.nproc, turn-1, latency, (len(senders), turn-1))
+        self.print_results(self.nproc, turn-1, biggest_latency, (len(msg_latencies), turn-1))
 
 
     def print_results(self, nproc, rounds, latency, throughput):
@@ -84,5 +92,25 @@ class Simulator:
         print '    Latency: ' + str(latency)
         print '    Throughput: ' + str(throughput[0]) + '/' + str(throughput[1])
 
-test = Simulator()
-test.simulate('BCTREE', 4)
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Simulate Broadcasts and Total Order Broadcast algorithms.')
+    parser.add_argument('filename', metavar='filename', type=str,
+                   help="""Settings file with parameters to simulate. See example in README""")
+
+    args = parser.parse_args()
+    settings = open(args.filename, 'r')
+    mode = settings.readline().strip()
+    nproc = int(settings.readline().strip())
+    schedule = [line.strip() for line in settings.readlines()]
+    new_msgs_schedule = []
+    for new_msg_timing in schedule:
+        turn, pid = new_msg_timing.split()
+        new_msgs_schedule.append((int(turn), int(pid)))
+    
+    simulator = Simulator()
+    simulator.simulate(mode, nproc, new_msgs_schedule)
+    
+main()
